7d441f0f419cb2b1ddc5173f9f0358d31fa164a0
Aramis Souza Farias <42254770+aramissf@users.noreply.github.com>
2022-02-15 15:17:20 -0300

    Add files via upload

---
+++ encontra.py
@@ -0,0 +1,63 @@
+import os
+
+
+caminho_procura = input('Infome um caminho : ')
+termo_procura = input('Digite um termo : ')
+
+def formata_tamanho(tamanho):
+    base = 1024
+    kilo = base
+    mega = base ** 2
+    giga = base ** 3
+    tera = base ** 4
+    peta = base ** 5
+
+    if tamanho < mega:
+        tamanho /= kilo
+        texto = 'K'
+    elif tamanho < giga:
+        tamanho /= mega
+        texto = 'M'
+    elif tamanho < tera:
+        tamanho /= giga
+        texto = 'G'
+    elif tamanho < peta:
+        tamanho /= tera
+        texto = 'T'
+    else :
+        tamanho /= peta
+        text = 'P'
+
+    tamanho = round(tamanho, 2)
+    return f'{tamanho} {texto}'.replace('.',',')
+
+
+
+
+
+conta = 0
+for raiz, diretorios, arquivos in os.walk(caminho_procura):
+    for arquivo in arquivos:
+        if termo_procura in arquivo:
+            try:
+                conta +=1
+                caminho_completo = os.path.join(raiz, arquivo)
+                nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                tamanho = os.path.getsize(caminho_completo)
+
+                print()
+                print('Encontrei o arquivo:' , arquivo)
+                print('Caminho :', caminho_completo)
+                print('Nome:', nome_arquivo)
+                print('Extensão:', ext_arquivo)
+                print('Tamanho:', tamanho)
+                print('Tamanho Fortado: ', formata_tamanho(tamanho))
+            except PermissionError as e:
+                print('Sem permissão')
+            except FileNotFoundError as e :
+                print('Arquivo não encontrado')
+            except Exception as e :
+                print('Erro desconhecido', e)
+
+print()
+print(f'{conta} arquivo(s) encontrado(s)')
